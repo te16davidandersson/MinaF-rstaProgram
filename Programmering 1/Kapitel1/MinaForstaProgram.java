0ed97430da2c286da8601c3354f1a478ff9c5ca4
David Andersson <anda16@nti.umea>
2017-09-11 11:15:28 +0200

    MinaForstaProgram

--- Programmering 1/Kapitel1/MinaForstaProgram.java
+++ Programmering 1/Kapitel1/MinaForstaProgram.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.*;
 
 public class MinaForstaProgram{
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null,"DavidAndersson");
-		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null,"070111111");
+		JOptionPane.showMessageDialog(null,"Hej" +
+			"\nVälkommen till mitt hus.");
+		JOptionPane.showMessageDialog(null,"070111111" +
+			"\nRing inte mig pls!");
+		JOptionPane.showMessageDialog(null, "Datum och tid: * \n" + Calendar.getInstance().getTime().toString());
+		//kommentar
 
 	}
 }
